d77ce0857469e7b647d209116af1d156b90b5d4d
JIToffolon <juanignaciotoffolon@gmail.com>
2022-12-06 23:23:58 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+/*
+1-Pedir Nombre
+2-Cartel de bienvenido ${Nombre}
+3-Preguntar Ganancia por hora
+4-Preguntar Horas Trabajadas por mes
+5-Preguntar si trabaja en blanco o negro
+*/
+let nombre=prompt("ingresa tu nombre aquí");
+let bienvenida=alert("Bienvenido: " + nombre);
+let gananciaPorhora=prompt(nombre + " " + "Ingresa cuanto ganas por hora" );
